5b10a5179576ab8b60f910425aa5b5780e2c9425
czy <1990724437@qq.com>
2020-05-11 10:59:00 +0800

    Test

---
+++ core_second/src/main/java/com/example/core_second/TestCore22.kt
@@ -0,0 +1,18 @@
+package com.example.core_second
+
+import android.util.Log
+
+/**
+ * 作者：leavesC
+ * 时间：2020/5/5 22:18
+ * 描述：
+ * GitHub：https://github.com/leavesC
+ * Blog：https://juejin.im/user/57c2ea9befa631005abd00c6
+ */
+object TestCore22 {
+
+    fun print(msg: String) {
+        Log.e("TAG", msg)
+    }
+
+}
